225a2b039e1c22102b73c8b6e9d8a294c48f793d
Sylar <vergil047@gmail.com>
2019-04-24 16:47:49 +0300

    Добавил функцию получения записей из лекции.

--- utils/db.php
+++ utils/db.php
@@ -61,3 +61,18 @@ function get_lots($con)
 
     return ['result' => $result, 'error' => $error];
 }
+
+// Вспомогательная функция получения записей.
+function db_fetch_data($link, $sql, $data = [])
+{
+    $result = [];
+    $stmt = db_get_prepare_stmt($link, $sql, $data);
+    mysqli_stmt_execute($stmt);
+    $res = mysqli_stmt_get_result($stmt);
+
+    if ($res) {
+        $result = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    }
+
+    return $result;
+}
