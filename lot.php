81d215c9e84840009e9cd30e870ac1f42f88388f
SylarWesker <vergil047@gmail.com>
2019-05-04 17:54:04 +0300

    Добавлена проверка параметра id (число ли передано).

--- lot.php
+++ lot.php
@@ -5,9 +5,6 @@ require_once('utils/db_helper.php');
 
 use yeticave\db\functions as db_func;
 
-// echo 'lot page';
-// echo "<br>";
-
 // эти параметры должны браться откуда из другого места...
 // пока просто закопирую.
 $user_name = 'Sylar';
@@ -21,43 +18,43 @@ $title = '';
 // категории заново получаем? ну вроде да...
 $stuff_categories = [];
 
+ // Получение списка категорий.
+ $func_result = db_func\get_stuff_categories($con);
+ $stuff_categories = $func_result['result'] === null ? [] : $func_result['result']; 
+
+ if ($func_result['error'] !== null) {
+     $errors[] = 'Ошибка MySql при получении списка категорий: ' . $func_result['error'];  
+ }
+
 if (isset($_GET['id'])) {
     // проверяю является ли числом (ну или просто сразу пытаюсь привести к числу)
-    $id = $_GET['id'];
+    if (is_numeric($_GET['id'])) {
+        $id = intval($_GET['id']);
 
-    // Берем лот по id.
-    $func_result = db_func\get_lots($con, [ $id ]);
-    $lot = $func_result['result'] === null ? [] : $func_result['result'][0]; 
+        // Берем лот по id.
+        $func_result = db_func\get_lots($con, [ $id ]);
+        $lot = $func_result['result'] === null ? [] : $func_result['result'][0]; 
 
-    // echo "<pre>";
-    // var_dump($lot);
-    // echo "</pre>";
-
-    if ($func_result['error'] !== null) {
-        $errors[] = $lot['error'];
-    }
-    else {
-        if ($lot === null) {
-            $errors[] = 'Не найден лот с id = ' . $id;
-        } 
-    }
-
-    // Получение списка категорий.
-    $func_result = db_func\get_stuff_categories($con);
-    $stuff_categories = $func_result['result'] === null ? [] : $func_result['result']; 
- 
-    if ($func_result['error'] !== null) {
-        $errors[] = 'Ошибка MySql при получении списка категорий: ' . $func_result['error'];  
+        if ($func_result['error'] !== null) {
+            $errors[] = $lot['error'];
+        } else {
+            if ($lot === null) {
+                $errors[] = 'Не найден лот с id = ' . $id;
+            } 
+        }
+    } else {
+        $errors[] = 'Параметр id не верного формата. Должно быть целое число.';
     }
 } else {
     $errors[] = 'Не передан параметр id!';
 }
 
-if (count($errors) != 0) {
-    // показываем ошибки
-}
-
 $con = null;
+
+if (count($errors) != 0) {
+    // показываем 404 и ошибки
+    header('Location: pages/404.html');
+}
 
 $content = include_template('lot.php', ['stuff_categories' => $stuff_categories,
                                         'lot' => $lot
