1d7fde4737e926733708780037b41a5c1e747a1c
Sylar <vergil047@gmail.com>
2019-05-08 10:29:07 +0300

    Убрал из репозитория используемый файл с конфигурацией БД. Добавил файл пример.

--- db_config.php
+++ db_config.php
@@ -2,10 +2,6 @@
 
 namespace yeticave\db\config;
 
-// ToDo
-// - Поместить файл конфигурации в gitignore (чтобы он не попал в git)
-// - Сделать такой же файл пример с фейковыми данными.
-
  return [
     'host' => 'localhost',
     'user' => 'yeticave_web',
