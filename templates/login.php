11ab5d6423894fa7620fb30dd882b49e64fad343
SylarWesker <vergil047@gmail.com>
2019-05-11 12:25:10 +0300

    Доделал вывод сообщений на странице авторизации.

--- templates/login.php
+++ templates/login.php
@@ -4,6 +4,12 @@ require_once('utils/utils.php');
 
 $has_errors = count($errors) > 0;
 
+// ToDo
+// Классы повторяются. 
+// -сделать константами и в отдельный файл???
+$form_invalid_class = 'form--invalid';
+$form_item_invalid_class = 'form__item--invalid';
+
 ?>
 
 <main>
@@ -29,17 +35,17 @@ $has_errors = count($errors) > 0;
       </li>
     </ul>
   </nav>
-  <form class="form container" action="login.php" method="post"> <!-- form--invalid -->
+  <form class="form container <?php if($has_errors) echo $form_invalid_class; ?>" action="login.php" method="post"> <!-- form--invalid -->
     <h2>Вход</h2>
-    <div class="form__item"> <!-- form__item--invalid -->
+    <div class="form__item <?php if(isset($errors['email'])) echo $form_item_invalid_class; ?>"> <!-- form__item--invalid -->
       <label for="email">E-mail <sup>*</sup></label>
-      <input id="email" type="text" name="email" placeholder="Введите e-mail">
-      <span class="form__error">Введите e-mail</span>
+      <input id="email" type="text" name="email" placeholder="Введите e-mail" value="<?= show_form_data('email', $form_data, $errors); ?>">
+      <span class="form__error"><?= show_error('email', $errors); ?></span>
     </div>
-    <div class="form__item form__item--last">
+    <div class="form__item form__item--last <?php if(isset($errors['password'])) echo $form_item_invalid_class; ?>">
       <label for="password">Пароль <sup>*</sup></label>
       <input id="password" type="password" name="password" placeholder="Введите пароль">
-      <span class="form__error">Введите пароль</span>
+      <span class="form__error"><?= show_error('password', $errors); ?></span>
     </div>
     <button type="submit" name="submit" class="button">Войти</button>
   </form>
