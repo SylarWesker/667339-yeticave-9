ebbefab01b019817943c1983bf33dee2c99f1e3e
Sylar <vergil047@gmail.com>
2019-05-23 08:05:19 +0300

    Убрал db_config из gitignore (надоело, что при переключении веток иногда пропадает конфиг. потом разберусь.)

---
+++ db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace yeticave\db\config;
+
+// Пример файла конфигурации БД.   
+ return [
+    'host' => 'localhost',
+    'user' => 'yeticave_web',
+    'password' => '123',
+    'db_name' => 'yeticave'
+];
