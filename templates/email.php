5c549f84c619d315fc7747ba6465921acaa65faa
SylarWesker <vergil047@gmail.com>
2019-05-26 12:17:51 +0300

    Добавил путь к серверу в тексте письма для победителя.

--- templates/email.php
+++ templates/email.php
@@ -1,12 +1,14 @@
 <?php
-    // ToDo
     // Добавить в пути ссылку на адрес сервера (имя сервера)
     // yeticave.localhost
+
+    // $server_name = $_SERVER['SERVER_NAME']; // в документации сказано, что нельзя доверять... Почему?
+    $server_name = 'yeticave.localhost';
 ?>
 
 <h1>Поздравляем с победой</h1>
 <p>Здравствуйте, <?= $user_name ?></p>
-<p>Ваша ставка для лота <a href="lot.php?id=<?= $lot_id ?>"><?= $lot_name ?></a> победила.</p>
-<p>Перейдите по ссылке <a href="my-bets.php">мои ставки</a>,
+<p>Ваша ставка для лота <a href="<?=$server_name?>/lot.php?id=<?= $lot_id ?>"><?= $lot_name ?></a> победила.</p>
+<p>Перейдите по ссылке <a href="<?=$server_name?>/my-bets.php">мои ставки</a>,
 чтобы связаться с автором объявления</p>
 <small>Интернет Аукцион "YetiCave"</small>
