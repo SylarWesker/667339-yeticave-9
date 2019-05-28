b452325d4076a389add59a335d18d0a1789955bf
SylarWesker <vergil047@gmail.com>
2019-05-28 22:52:01 +0300

    Исправлена ошибка. Указан правильный путь к странице login a.

--- sign-up.php
+++ sign-up.php
@@ -62,7 +62,7 @@ if (isset($_POST['submit'])) {
 
         // Редирект на страницу авторизации.
         if ($no_errors) {
-            $login_page = 'pages/login.php';
+            $login_page = 'login.php';
 
             header('Location: ' . $login_page);
         } else {
